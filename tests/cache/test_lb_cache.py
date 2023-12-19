a7b54e3e0e43c243e3d487ca84ea6ec79bec26fa
Alex Hoffmann <alhoffmann@gmail.com>
2023-12-19 13:04:25 +0400

    proper testing setup

--- tests/cache/test_lb_cache.py
+++ tests/cache/test_lb_cache.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import asyncio
 import os
-from pathlib import Path
+import tempfile
 
 import diskcache
 import pytest
@@ -9,14 +9,13 @@ from pytest_mock import MockerFixture
 
 from lbgpt import ChatGPT
 from lbgpt.types import ChatCompletionAddition
-import tempfile
 
-CACHES = [
-    diskcache.Cache(Path(__file__).parent / "_files/diskcache")
-]
+
+CACHES = [diskcache.Cache(tempfile.TemporaryDirectory().name)]
 
 
 @pytest.mark.parametrize("cache", CACHES)
+@pytest.mark.vcr
 def test_chatgpt_cache(mocker: MockerFixture, cache):
     messages = [
         {"role": "user", "content": "please respond with pong"},
@@ -36,30 +35,33 @@ def test_chatgpt_cache(mocker: MockerFixture, cache):
         api_key=os.environ["OPEN_AI_API_KEY"],
         stop_after_attempts=1,
         stop_on_exception=True,
-        cache=cache
+        cache=cache,
     )
 
+    # Setting the cache
+    cache_interaction = mocker.spy(lb, "_request_from_cache")
+
+    # run with an empty cache
+    asyncio.run(lb.chat_completion_list([single_request_content], show_progress=False))
+
+    # asserting that the cache was not called
+    assert cache_interaction.call_count == 1
+    assert cache_interaction.spy_return is None
+
     # some cache stats and hash
-    cache_stats = {
-        'hash': hash(cache)
-    }
+    cache_stats = {"hash": hash(cache)}
 
-    if hasattr(cache, 'count'):
-        cache_stats['count'] = cache.count
+    if hasattr(cache, "count"):
+        cache_stats["count"] = cache.count
 
-
-    openai = mocker.spy(lb, "_request_from_cache")
-
+    # Getting from cache
     asyncio.run(lb.chat_completion_list([single_request_content], show_progress=False))
 
     # asserting that the cache was called and returned the values
-    assert openai.call_count == 1
-    assert isinstance(openai.spy_return, ChatCompletionAddition)
+    assert cache_interaction.call_count == 2
+    assert isinstance(cache_interaction.spy_return, ChatCompletionAddition)
 
     # asserting that no items were added to the cache
-    if hasattr(cache, 'count'):
-        assert cache.count == cache_stats['count']
-    assert hash(cache) == cache_stats['hash']
-
-
-
+    if hasattr(cache, "count"):
+        assert cache.count == cache_stats["count"]
+    assert hash(cache) == cache_stats["hash"]
---
+++ tests/conftest.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import os
+from typing import Any, Dict
+
+import pytest
+import vcr  # type: ignore[import]
+import vcr.request  # type: ignore[import]
+import vcr.util  # type: ignore[import]
+
+
+vcr_match_on = ("method", "scheme", "host", "port", "path", "query")
+
+
+@pytest.fixture()
+def vcr_cassette_dir(request):  # type: ignore[no-untyped-def]
+    return os.path.join(
+        os.path.dirname(request.module.__file__), "cassette", request.module.__name__
+    )
+
+
+@pytest.fixture()
+def vcr_config() -> Dict[str, Any]:
+    return {
+        "record_mode": "once",  # new_episodes
+        "match_on": vcr_match_on,
+        "filter_headers": {"authorization": "DUMMY_AUTHORIZATION"},
+        "filter_query_parameters": ["token"],
+        "ignore_hosts": ["localhost", "testserver"],
+    }
+
+
