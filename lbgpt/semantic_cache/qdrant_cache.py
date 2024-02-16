09c12cc53472ef02d002b9f618b6f5f50311c5bc
Alex Hoffmann <alhoffmann@gmail.com>
2024-02-16 12:55:27 +0400

    timeout and error handling for qdrant cache

--- lbgpt/semantic_cache/qdrant_cache.py
+++ lbgpt/semantic_cache/qdrant_cache.py
@@ -1,8 +1,6 @@
 import uuid
 from types import NoneType
 from typing import Any, Optional
-
-import tenacity
 from langchain_core.embeddings import Embeddings
 from openai.types.chat import ChatCompletion, CompletionCreateParams
 from pydantic import BaseModel
@@ -10,10 +8,14 @@ from qdrant_client import AsyncQdrantClient, QdrantClient
 from qdrant_client.http.exceptions import ResponseHandlingException
 from qdrant_client.http.models import FieldCondition, Filter, MatchValue, PointStruct
 from qdrant_client.models import Distance, VectorParams
-from tenacity import retry_if_exception_type, wait_random_exponential
-
 from lbgpt.semantic_cache.base import _SemanticCacheBase, get_completion_create_params
 from lbgpt.types import ChatCompletionAddition
+
+
+from logging import getLogger
+
+
+logger = getLogger(__name__)
 
 
 class QdrantClientConfig(BaseModel):
@@ -32,7 +34,11 @@ class QdrantClientConfig(BaseModel):
             api_key = self.api_key
 
         return AsyncQdrantClient(
-            host=self.host, port=self.port, url=self.url, api_key=api_key, **self.extra_fields
+            host=self.host,
+            port=self.port,
+            url=self.url,
+            api_key=api_key,
+            **self.extra_fields,
         )
 
     def get_sync_client(self) -> QdrantClient:
@@ -41,17 +47,17 @@ class QdrantClientConfig(BaseModel):
         else:
             api_key = self.api_key
         return QdrantClient(
-            host=self.host, port=self.port, url=self.url, api_key=api_key, **self.extra_fields
+            host=self.host,
+            port=self.port,
+            url=self.url,
+            api_key=api_key,
+            **self.extra_fields,
         )
 
     @property
     def extra_fields(self) -> dict[str, Any]:
         field_names = set(self.__dict__) - set(self.__fields__)
-        return {
-            fn: getattr(self, fn)
-            for fn in field_names
-        }
-
+        return {fn: getattr(self, fn) for fn in field_names}
 
 
 class QdrantSemanticCache(_SemanticCacheBase):
@@ -101,7 +107,6 @@ class QdrantSemanticCache(_SemanticCacheBase):
             )
         client.close()
 
-
     def _create_filter_value(
         self, value: int | str | bool | NoneType
     ) -> int | str | bool:
@@ -112,11 +117,27 @@ class QdrantSemanticCache(_SemanticCacheBase):
         else:
             raise NotImplementedError(f"cannot process type {type(value)}")
 
-    @tenacity.retry(
-        retry=(retry_if_exception_type(ResponseHandlingException)),
-        wait=wait_random_exponential(min=5, max=60),
-    )
     async def query_cache(
+        self, query: CompletionCreateParams | dict[str, Any]
+    ) -> Optional[ChatCompletionAddition]:
+        try:
+            return await self._query_cache(query=query)
+        except ResponseHandlingException as e:
+            logger.debug(f"Error querying cache, proceed anyways: {e}")
+            # we don't really care about errors here
+            return
+
+    async def add_cache(
+        self, query: CompletionCreateParams | dict[str, Any], response: ChatCompletion
+    ) -> None:
+        try:
+            return await self._add_cache(query=query, response=response)
+        except ResponseHandlingException as e:
+            logger.debug(f"Error querying cache, proceed anyways: {e}")
+            # we don't really care about errors here
+            return
+
+    async def _query_cache(
         self, query: CompletionCreateParams | dict[str, Any]
     ) -> Optional[ChatCompletionAddition]:
         query = get_completion_create_params(**query)
@@ -143,6 +164,7 @@ class QdrantSemanticCache(_SemanticCacheBase):
             with_vectors=False,
             limit=1,
             score_threshold=self.cosine_similarity_threshold,
+            timeout=10,
         )
 
         res = [r for r in res if r.score >= self.cosine_similarity_threshold]
@@ -155,11 +177,7 @@ class QdrantSemanticCache(_SemanticCacheBase):
         else:
             return
 
-    @tenacity.retry(
-        retry=(retry_if_exception_type(ResponseHandlingException)),
-        wait=wait_random_exponential(min=5, max=60),
-    )
-    async def add_cache(
+    async def _add_cache(
         self, query: CompletionCreateParams | dict[str, Any], response: ChatCompletion
     ) -> None:
         query = get_completion_create_params(**query)
@@ -190,6 +208,8 @@ class QdrantSemanticCache(_SemanticCacheBase):
 
     @property
     def count(self) -> int:
-        return self._QdrantClientConfig.get_sync_client().count(
-            collection_name=self.collection_name
-        ).count
+        return (
+            self._QdrantClientConfig.get_sync_client()
+            .count(collection_name=self.collection_name)
+            .count
+        )
